71a9fbe11537bea4ce7dd8a0c22ae83d40864f44
Aditya <adityamore360123@gmail.com>
2021-09-04 22:05:06 +0530

    client side authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
-import { ChannelListContainer, ChannelContainer } from './components';
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
 
 import "./App.css"
 
@@ -12,7 +12,10 @@ const apiKey = "thw56b2ap33b";
 
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
 const App = () => {
+
+    if (!authToken) return <Auth />
     return (
         <div className="app__wrapper">
             <Chat client={client} theme="team light">
