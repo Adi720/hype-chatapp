43581e9f726a6fa6041d51fd2284a784831039e4
Aditya <adityamore360123@gmail.com>
2021-09-05 19:17:51 +0530

    server side authentication

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+require('dotenv').config();
+
+// MIDDLEWARE
+
+// Allows cross=origin requests
+app.use(cors());
+// Allows json payloads from frontend->backend
+app.use(express.json());
+
+app.use(express.urlencoded());
+
+// ROUTES
+
+app.get('/', (req, res) => {
+    res.send("Hello, World!");
+})
+
+app.use('./auth', authRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
